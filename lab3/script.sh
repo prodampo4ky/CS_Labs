a7a30ce951872bf9211155c73256e064fcdca4e3
prodampo4ky <bench100press@gmail.com>
2021-08-29 00:51:50 +0300

    Create script.sh

---
+++ lab3/script.sh
@@ -0,0 +1,38 @@
+﻿#!/bin/bash
+
+ml icc
+echo "g++ with O"
+g++ -O pr.cpp -o testO
+time ./testO
+echo " "
+echo "g++ with O1"
+g++ -O1 pr.cpp -o test1
+time ./test1
+echo " "
+echo "g++ with O2"
+g++ -O2 pr.cpp -o test2
+time ./test2
+echo " "
+echo "g++ with O3"
+g++ -O3 pr.cpp -o test3
+time ./test3
+echo " "
+echo "g++ with O fast"
+g++ -Ofast pr.cpp -o testFast
+time ./testFast
+echo " "
+grep 'flags' /proc/cpuinfo | uniq
+echo " "
+echo -e "Extentions:\n"
+echo "sse2"
+icc -std=c++11 -xsse2 -O2 pr.cpp -o ntest
+time ./n1test
+echo " "
+echo "ssse3"
+icc -std=c++11 -xssse3 -O2 pr.cpp -o ntest
+time ./n2test
+echo " "
+echo "avx"
+icc -std=c++11 -xavx -O2 pr.cpp -o ntest
+time ./n3test
+echo " "

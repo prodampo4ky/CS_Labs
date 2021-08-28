238602f007e13073071f4c4fdef2be526664e868
prodampo4ky <bench100press@gmail.com>
2021-08-29 01:06:57 +0300

    Update script.sh

--- lab3/script.sh
+++ lab3/script.sh
@@ -2,37 +2,37 @@
 
 ml icc
 echo "g++ with O"
-g++ -O pr.cpp -o testO
-time ./testO
+g++ -O lab3.cpp -o test0
+time ./test0
 echo " "
 echo "g++ with O1"
-g++ -O1 pr.cpp -o test1
+g++ -O1 lab3.cpp -o test1
 time ./test1
 echo " "
 echo "g++ with O2"
-g++ -O2 pr.cpp -o test2
+g++ -O2 lab3.cpp -o test2
 time ./test2
 echo " "
 echo "g++ with O3"
-g++ -O3 pr.cpp -o test3
+g++ -O3 lab3.cpp -o test3
 time ./test3
 echo " "
 echo "g++ with O fast"
-g++ -Ofast pr.cpp -o testFast
+g++ -Ofast lab3.cpp -o testFast
 time ./testFast
 echo " "
 grep 'flags' /proc/cpuinfo | uniq
 echo " "
 echo -e "Extentions:\n"
 echo "sse2"
-icc -std=c++11 -xsse2 -O2 pr.cpp -o ntest
+icc -std=c++11 -xsse2 -O2 lab3.cpp -o n1test
 time ./n1test
 echo " "
 echo "ssse3"
-icc -std=c++11 -xssse3 -O2 pr.cpp -o ntest
+icc -std=c++11 -xssse3 -O2 lab3.cpp -o n2test
 time ./n2test
 echo " "
 echo "avx"
-icc -std=c++11 -xavx -O2 pr.cpp -o ntest
+icc -std=c++11 -xavx -O2 lab3.cpp -o n3test
 time ./n3test
 echo " "
